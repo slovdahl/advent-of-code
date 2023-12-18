45cf8f924131843d62cfbdb30421bf62c6137e52
Sebastian Lövdahl <slovdahl@hibox.fi>
2023-12-18 18:30:25 +0200

    Year 2023: Day 13 part 2

--- year-2023/java/src/year2023/Day13.java
+++ year-2023/java/src/year2023/Day13.java
@@ -2,16 +2,20 @@ package year2023;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
 public class Day13 extends Day {
 
+    private List<List<char[]>> patterns;
+
     @Override
-    Integer part1(Stream<String> input) throws IOException {
+    void prepare(Stream<String> input) {
         List<String> inputLines = input.toList();
-        List<List<char[]>> patterns = new ArrayList<>();
+        patterns = new ArrayList<>();
 
         List<char[]> current = new ArrayList<>();
         for (String inputLine : inputLines) {
@@ -25,7 +29,10 @@ public class Day13 extends Day {
         }
 
         patterns.add(current);
+    }
 
+    @Override
+    Integer part1(Stream<String> input) throws IOException {
         int rowSummary = 0;
         int columnSummary = 0;
 
@@ -50,8 +57,6 @@ public class Day13 extends Day {
                     boolean mirror = areRowMirrors(row1, row2);
 
                     if (mirror) {
-                        System.out.println(i + ": FOUND INITIAL ROW MIRROR AT " + row1Index + " AND " + row2Index);
-
                         if (!foundInitialMirror) {
                             innerMostRowMirror1 = row1Index;
                             innerMostRowMirror2 = row2Index;
@@ -60,7 +65,6 @@ public class Day13 extends Day {
 
                         if (row1Index == 0 || row2Index + 1 == pattern.size()) {
                             hasFullRowMirror = true;
-                            System.out.println(i + ": FULL ROW MIRROR STARTING AT " + innerMostRowMirror1 + " AND " + innerMostRowMirror2);
                             break;
                         }
 
@@ -77,6 +81,7 @@ public class Day13 extends Day {
                         row1Index++;
                         row2Index++;
                     } else {
+                        innerMostRowMirror1 = innerMostRowMirror2 = -1;
                         break;
                     }
                 }
@@ -96,8 +101,6 @@ public class Day13 extends Day {
                     boolean mirror = areColumnMirrors(pattern, column1Index, column2Index);
 
                     if (mirror) {
-                        System.out.println(i + ": FOUND INITIAL COLUMN MIRROR AT " + column1Index + " AND " + column2Index);
-
                         if (!foundInitialMirror) {
                             innerMostColumnMirror1 = column1Index;
                             innerMostColumnMirror2 = column2Index;
@@ -106,7 +109,6 @@ public class Day13 extends Day {
 
                         if (column1Index == 0 || column2Index + 1 == pattern.getFirst().length) {
                             hasFullColumnMirror = true;
-                            System.out.println(i + ": FULL COLUMN MIRROR STARTING AT " + innerMostColumnMirror1 + " AND " + innerMostColumnMirror2);
                             break;
                         }
 
@@ -123,6 +125,7 @@ public class Day13 extends Day {
                         column1Index++;
                         column2Index++;
                     } else {
+                        innerMostColumnMirror1 = innerMostColumnMirror2 = -1;
                         break;
                     }
                 }
@@ -138,6 +141,256 @@ public class Day13 extends Day {
         }
 
         return rowSummary + columnSummary; // Your puzzle answer was 34202
+    }
+
+    @Override
+    Integer part2(Stream<String> input) throws Exception {
+        Map<Integer, Integer> lastHalfRowPartOfMirror = new LinkedHashMap<>();
+        Map<Integer, Integer> lastHalfColumnPartOfMirror = new LinkedHashMap<>();
+
+        for (int i = 0; i < patterns.size(); i++) {
+            List<char[]> pattern = patterns.get(i);
+
+            // rows
+            boolean hasFullRowMirror = false;
+            int innerMostRowMirror1 = -1;
+            int innerMostRowMirror2 = -1;
+
+            {
+                int row1Index = 0;
+                int row2Index = 1;
+
+                boolean foundInitialMirror = false;
+
+                while (true) {
+                    char[] row1 = pattern.get(row1Index);
+                    char[] row2 = pattern.get(row2Index);
+
+                    boolean mirror = areRowMirrors(row1, row2);
+
+                    if (mirror) {
+                        if (!foundInitialMirror) {
+                            innerMostRowMirror1 = row1Index;
+                            innerMostRowMirror2 = row2Index;
+                            foundInitialMirror = true;
+                        }
+
+                        if (row1Index == 0 || row2Index + 1 == pattern.size()) {
+                            hasFullRowMirror = true;
+                            lastHalfRowPartOfMirror.put(i, innerMostRowMirror1);
+                            break;
+                        }
+
+                        row1Index--;
+                        row2Index++;
+                    } else if (!mirror && foundInitialMirror) {
+                        if (innerMostRowMirror2 + 1 < pattern.size()) {
+                            row1Index = innerMostRowMirror1 + 1;
+                            row2Index = innerMostRowMirror2 + 1;
+                        }
+                        innerMostRowMirror1 = innerMostRowMirror2 = -1;
+                        foundInitialMirror = false;
+                    } else if (row2Index + 1 < pattern.size()) {
+                        row1Index++;
+                        row2Index++;
+                    } else {
+                        innerMostRowMirror1 = innerMostRowMirror2 = -1;
+                        break;
+                    }
+                }
+            }
+
+            // columns
+            boolean hasFullColumnMirror = false;
+            int innerMostColumnMirror1 = -1;
+            int innerMostColumnMirror2 = -1;
+
+            if (!hasFullRowMirror) {
+                int column1Index = 0;
+                int column2Index = 1;
+                boolean foundInitialMirror = false;
+
+                while (true) {
+                    boolean mirror = areColumnMirrors(pattern, column1Index, column2Index);
+
+                    if (mirror) {
+                        if (!foundInitialMirror) {
+                            innerMostColumnMirror1 = column1Index;
+                            innerMostColumnMirror2 = column2Index;
+                            foundInitialMirror = true;
+                        }
+
+                        if (column1Index == 0 || column2Index + 1 == pattern.getFirst().length) {
+                            hasFullColumnMirror = true;
+                            lastHalfColumnPartOfMirror.put(i, innerMostColumnMirror1);
+                            break;
+                        }
+
+                        column1Index--;
+                        column2Index++;
+                    } else if (!mirror && foundInitialMirror) {
+                        if (innerMostColumnMirror2 + 1 < pattern.getFirst().length) {
+                            column1Index = innerMostColumnMirror1 + 1;
+                            column2Index = innerMostColumnMirror2 + 1;
+                        }
+                        innerMostColumnMirror1 = innerMostColumnMirror2 = -1;
+                        foundInitialMirror = false;
+                    } else if (column2Index + 1 < pattern.getFirst().length) {
+                        column1Index++;
+                        column2Index++;
+                    } else {
+                        innerMostColumnMirror1 = innerMostColumnMirror2 = -1;
+                        break;
+                    }
+                }
+            }
+
+            if (!hasFullRowMirror && !hasFullColumnMirror) {
+                throw new IllegalStateException("neither row nor column mirror found");
+            }
+        }
+
+        int rowSummary = 0;
+        int columnSummary = 0;
+
+        for (int i = 0; i < patterns.size(); i++) {
+            List<char[]> pattern = patterns.get(i);
+
+            boolean hasFullRowMirror = false;
+            int innerMostRowMirror1 = -1;
+            int innerMostRowMirror2 = -1;
+
+            boolean hasFullColumnMirror = false;
+            int innerMostColumnMirror1 = -1;
+            int innerMostColumnMirror2 = -1;
+
+            outer:
+            for (int row = 0; row < pattern.size(); row++) {
+                for (int column = 0; column < pattern.get(row).length; column++) {
+                    char previous = pattern.get(row)[column];
+                    pattern.get(row)[column] = previous == '.' ? '#' : '.';
+
+                    innerMostRowMirror1 = innerMostRowMirror2 = -1;
+                    innerMostColumnMirror1 = innerMostColumnMirror2 = -1;
+
+                    // rows
+                    {
+                        int row1Index = 0;
+                        int row2Index = 1;
+
+                        boolean foundInitialMirror = false;
+
+                        while (true) {
+                            if (lastHalfRowPartOfMirror.getOrDefault(i, -1) == row1Index) {
+                                if (row2Index + 1 < pattern.size()) {
+                                    row1Index++;
+                                    row2Index++;
+                                } else {
+                                    break;
+                                }
+                            }
+
+                            char[] row1 = pattern.get(row1Index);
+                            char[] row2 = pattern.get(row2Index);
+
+                            boolean mirror = areRowMirrors(row1, row2);
+
+                            if (mirror) {
+                                if (!foundInitialMirror) {
+                                    innerMostRowMirror1 = row1Index;
+                                    innerMostRowMirror2 = row2Index;
+                                    foundInitialMirror = true;
+                                }
+
+                                if (row1Index == 0 || row2Index + 1 == pattern.size()) {
+                                    hasFullRowMirror = true;
+                                    break;
+                                }
+
+                                row1Index--;
+                                row2Index++;
+                            } else if (!mirror && foundInitialMirror) {
+                                if (innerMostRowMirror2 + 1 < pattern.size()) {
+                                    row1Index = innerMostRowMirror1 + 1;
+                                    row2Index = innerMostRowMirror2 + 1;
+                                }
+                                innerMostRowMirror1 = innerMostRowMirror2 = -1;
+                                foundInitialMirror = false;
+                            } else if (row2Index + 1 < pattern.size()) {
+                                row1Index++;
+                                row2Index++;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    // columns
+                    if (!hasFullRowMirror) {
+                        int column1Index = 0;
+                        int column2Index = 1;
+                        boolean foundInitialMirror = false;
+
+                        while (true) {
+                            if (lastHalfColumnPartOfMirror.getOrDefault(i, -1) == column1Index) {
+                                if (column2Index + 1 < pattern.getFirst().length) {
+                                    column1Index++;
+                                    column2Index++;
+                                } else {
+                                    break;
+                                }
+                            }
+
+                            boolean mirror = areColumnMirrors(pattern, column1Index, column2Index);
+
+                            if (mirror) {
+                                if (!foundInitialMirror) {
+                                    innerMostColumnMirror1 = column1Index;
+                                    innerMostColumnMirror2 = column2Index;
+                                    foundInitialMirror = true;
+                                }
+
+                                if (column1Index == 0 || column2Index + 1 == pattern.getFirst().length) {
+                                    hasFullColumnMirror = true;
+                                    break;
+                                }
+
+                                column1Index--;
+                                column2Index++;
+                            } else if (!mirror && foundInitialMirror) {
+                                if (innerMostColumnMirror2 + 1 < pattern.getFirst().length) {
+                                    column1Index = innerMostColumnMirror1 + 1;
+                                    column2Index = innerMostColumnMirror2 + 1;
+                                }
+                                innerMostColumnMirror1 = innerMostColumnMirror2 = -1;
+                                foundInitialMirror = false;
+                            } else if (column2Index + 1 < pattern.getFirst().length) {
+                                column1Index++;
+                                column2Index++;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (hasFullRowMirror || hasFullColumnMirror) {
+                        break outer;
+                    } else {
+                        pattern.get(row)[column] = previous;
+                    }
+                }
+            }
+
+            if (hasFullRowMirror) {
+                rowSummary += (innerMostRowMirror1 + 1) * 100;
+            } else if (hasFullColumnMirror) {
+                columnSummary += innerMostColumnMirror1 + 1;
+            } else {
+                throw new IllegalStateException("neither row nor column mirror found");
+            }
+        }
+
+        return rowSummary + columnSummary; // Your puzzle answer was 34230
     }
 
     private static boolean areRowMirrors(char[] row1, char[] row2) {
