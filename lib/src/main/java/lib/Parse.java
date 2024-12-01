4907c131e418f9762de46d87fd54c273a84f5830
Sebastian Lövdahl <slovdahl@hibox.fi>
2024-12-01 09:52:34 +0200

    Year 2024: Day 1 part 1

--- lib/src/main/java/lib/Parse.java
+++ lib/src/main/java/lib/Parse.java
@@ -7,12 +7,18 @@ import java.util.regex.Pattern;
 public class Parse {
     private static final Pattern SPACE_PATTERN = Pattern.compile("\\s+");
 
+    /**
+     * Parses the input as space-separated integers.
+     */
     public static List<Integer> ints(String input) {
         return Arrays.stream(SPACE_PATTERN.split(input.trim()))
                 .map(Integer::parseInt)
                 .toList();
     }
 
+    /**
+     * Parses the input as space-separated longs.
+     */
     public static List<Long> longs(String input) {
         return Arrays.stream(SPACE_PATTERN.split(input.trim()))
                 .map(Long::parseLong)
--- year-2024/src/main/java/year2024/Day1.java
+++ year-2024/src/main/java/year2024/Day1.java
@@ -1,7 +1,11 @@
 package year2024;
 
 import lib.Day;
+import lib.Parse;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
@@ -9,6 +13,33 @@ public class Day1 extends Day {
 
     @Override
     protected Object part1(Stream<String> input) {
-        return null;
+        List<List<Integer>> list = input
+                .map(Parse::ints)
+                .toList();
+
+        List<Integer> firstColumn = new ArrayList<>();
+        List<Integer> secondColumn = new ArrayList<>();
+
+        for (List<Integer> row : list) {
+            firstColumn.add(row.get(0));
+            secondColumn.add(row.get(1));
+        }
+
+        firstColumn.sort(Integer::compareTo);
+        secondColumn.sort(Integer::compareTo);
+
+        int sum = 0;
+
+        Iterator<Integer> firstIterator = firstColumn.iterator();
+        Iterator<Integer> secondIterator = secondColumn.iterator();
+
+        while (firstIterator.hasNext() && secondIterator.hasNext()) {
+            Integer first = firstIterator.next();
+            Integer second = secondIterator.next();
+            int distance = Math.abs(second - first);
+            sum += distance;
+        }
+
+        return sum; // Your puzzle answer was 2164381
     }
 }
