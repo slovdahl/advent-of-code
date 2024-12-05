067916b10735039fb29aea0f1a32eb2b82516c6c
Sebastian Lövdahl <slovdahl@hibox.fi>
2024-12-05 09:44:34 +0200

    Year 2024: Day 5 part 2

--- year-2024/src/main/java/year2024/Day5.java
+++ year-2024/src/main/java/year2024/Day5.java
@@ -5,6 +5,7 @@ import com.google.common.collect.SetMultimap;
 import lib.Day;
 import lib.Parse;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -15,22 +16,28 @@ import java.util.stream.Stream;
 @SuppressWarnings("unused")
 public class Day5 extends Day {
 
+    private List<Rule> rules;
+    private SetMultimap<Integer, Integer> rulePerNumber;
+
+    private List<List<Integer>> invalidUpdates;
+    private List<List<Integer>> validUpdates;
+
     @Override
     protected Mode mode() {
         return Mode.REAL_INPUT;
     }
 
     @Override
-    protected Object part1(Stream<String> input) {
+    protected void prepare(Stream<String> input) throws Exception {
         List<String> fullInput = input.toList();
 
-        List<Rule> rules = fullInput.stream()
+        rules = fullInput.stream()
                 .takeWhile(line -> !line.isEmpty())
                 .map(line -> line.split("\\|"))
                 .map(pair -> new Rule(Integer.parseInt(pair[0]), Integer.parseInt(pair[1])))
                 .toList();
 
-        SetMultimap<Integer, Integer> rulePerNumber = HashMultimap.create();
+        rulePerNumber = HashMultimap.create();
         for (Rule rule : rules) {
             rulePerNumber.put(rule.before(), rule.after());
         }
@@ -40,6 +47,8 @@ public class Day5 extends Day {
                 .filter(line -> !line.isEmpty())
                 .map(Parse::commaSeparatedInts)
                 .collect(Collectors.toList());
+
+        List<List<Integer>> invalidUpdates = new ArrayList<>();
 
         Iterator<List<Integer>> iterator = updateLines.iterator();
         while (iterator.hasNext()) {
@@ -52,6 +61,7 @@ public class Day5 extends Day {
 
                 for (Integer s : seen) {
                     if (shouldComeAfter.contains(s)) {
+                        invalidUpdates.add(updateLine);
                         iterator.remove();
                         break outer;
                     }
@@ -61,13 +71,53 @@ public class Day5 extends Day {
             }
         }
 
+        this.invalidUpdates = List.copyOf(invalidUpdates);
+        validUpdates = List.copyOf(updateLines);
+    }
+
+    @Override
+    protected Object part1(Stream<String> input) {
         int sum = 0;
-        for (List<Integer> updateLine : updateLines) {
+        for (List<Integer> updateLine : validUpdates) {
             int middleIndex = updateLine.size() / 2;
             sum += updateLine.get(middleIndex);
         }
 
         return sum; // Your puzzle answer was 4766
+    }
+
+    @Override
+    protected Object part2(Stream<String> input) {
+        int sum = 0;
+        for (List<Integer> invalidUpdate : invalidUpdates) {
+            List<Integer> updateLine = new ArrayList<>(invalidUpdate);
+            Set<Integer> seen = new HashSet<>();
+
+            outer:
+            while (true) {
+                for (int i = 0; i < updateLine.size(); i++) {
+                    Integer n = updateLine.get(i);
+                    Set<Integer> shouldComeAfter = rulePerNumber.get(n);
+
+                    for (Integer s : seen) {
+                        if (shouldComeAfter.contains(s)) {
+                            Integer previous = updateLine.get(i - 1);
+                            updateLine.set(i - 1, n);
+                            updateLine.set(i, previous);
+                            seen.clear();
+                            continue outer;
+                        }
+                    }
+                    seen.add(n);
+                }
+
+                int middleIndex = updateLine.size() / 2;
+                sum += updateLine.get(middleIndex);
+                break;
+            }
+        }
+
+        return sum; // Your puzzle answer was 6257
     }
 
     private record Rule(int before, int after) {
