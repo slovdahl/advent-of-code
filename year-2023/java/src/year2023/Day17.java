a6c3ab2a118db8d36872d04078c334816d8955ec
Sebastian Lövdahl <slovdahl@hibox.fi>
2024-12-06 21:40:23 +0200

    Year 2023: Small cleanups

--- year-2023/java/src/year2023/Day17.java
+++ year-2023/java/src/year2023/Day17.java
@@ -30,22 +30,6 @@ public class Day17 extends Day {
 
     @Override
     protected Integer part1(Stream<String> input) throws IOException {
-        Stream<String> sampleInput = """
-                2413432311323
-                3215453535623
-                3255245654254
-                3446585845452
-                4546657867536
-                1438598798454
-                4457876987766
-                3637877979653
-                4654967986887
-                4564679986453
-                1224686865563
-                2546548887735
-                4322674655533
-                """.lines();
-
         int[][] matrix = intMatrix(input.toList());
 
         int goalRow = matrix.length;
