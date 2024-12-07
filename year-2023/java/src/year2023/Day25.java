eae876a0620182325be49206c30156c827877538
Sebastian Lövdahl <slovdahl@hibox.fi>
2024-12-07 13:39:43 +0200

    WIP

--- year-2023/java/src/year2023/Day25.java
+++ year-2023/java/src/year2023/Day25.java
@@ -6,9 +6,14 @@ import com.google.common.graph.MutableGraph;
 import com.google.common.graph.Traverser;
 
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toMap;
 import static java.util.stream.Collectors.toUnmodifiableMap;
 import static year2023.Common.visualizeAsDot;
 
@@ -36,6 +41,22 @@ public class Day25 extends Day {
         Map<String, String[]> nameToConnections = input
                 .map(line -> line.split(":"))
                 .collect(toUnmodifiableMap(pair -> pair[0], pair -> pair[1].trim().split(" ")));
+
+        Map<String, Component> components = nameToConnections.keySet().stream()
+                .map(name -> new Component(name, new HashSet<>()))
+                .collect(toMap(Component::name, component -> component));
+
+        for (Map.Entry<String, String[]> entry : nameToConnections.entrySet()) {
+            components.get(entry.getKey()).connections().addAll(
+                    Arrays.stream(entry.getValue())
+                            .map(component -> components.computeIfAbsent(component, c -> new Component(c, new HashSet<>())))
+                            .toList()
+            );
+        }
+
+        Component componentWithMostLinks = components.values().stream()
+                .max(comparing(Component::numberOfLinks))
+                .orElseThrow();
 
         MutableGraph<String> graph = GraphBuilder.undirected()
                 .allowsSelfLoops(false)
@@ -65,5 +86,16 @@ public class Day25 extends Day {
 
         return Iterators.size(Traverser.forGraph(graph).breadthFirst("sph").iterator()) *
                 Iterators.size(Traverser.forGraph(graph).breadthFirst("rkh").iterator()); // Your puzzle answer was 614655
+    }
+
+    private record Component(String name, Set<Component> connections) {
+
+        int numberOfLinks() {
+            int n = connections.size();
+            for (Component connection : connections) {
+                n += connection.numberOfLinks();
+            }
+            return n;
+        }
     }
 }
