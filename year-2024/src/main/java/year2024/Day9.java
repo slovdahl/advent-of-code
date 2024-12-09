c2f3f9070734dc05c8914f62f0841f0072bdccf1
Sebastian Lövdahl <slovdahl@hibox.fi>
2024-12-09 12:19:31 +0200

    Year 2024: Day 9 part 2

--- year-2024/src/main/java/year2024/Day9.java
+++ year-2024/src/main/java/year2024/Day9.java
@@ -1,10 +1,15 @@
 package year2024;
 
 import lib.Day;
+import lib.Pair;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
@@ -23,16 +28,15 @@ public class Day9 extends Day {
         int fileId = 0;
         List<Block> blocks = new ArrayList<>();
         for (int i = 0; i < charArray.length; i++) {
-            int digit = Character.digit(charArray[i], 10);
+            int size = Character.digit(charArray[i], 10);
             if (i % 2 == 0) {
-                for (int n = 0; n < digit; n++) {
-                    blocks.add(new FileBlock(fileId));
+                for (int n = 0; n < size; n++) {
+                    blocks.add(new FileBlock(fileId, size));
                 }
                 fileId++;
-            }
-            else {
-                for (int n = 0; n < digit; n++) {
-                    blocks.add(new EmptySpace());
+            } else {
+                for (int n = 0; n < size; n++) {
+                    blocks.add(new EmptySpace(size));
                 }
             }
         }
@@ -55,15 +59,136 @@ public class Day9 extends Day {
             }
         }
 
-        long checksum = 0;
-        for (int i = 0; i < blocks.size(); i++) {
-            Block block = blocks.get(i);
-            if (block instanceof FileBlock(int id)) {
-                checksum += (long) id * i;
+        return calculateChecksum(blocks); // Your puzzle answer was 6382875730645
+    }
+
+    @Override
+    protected Object part2(Stream<String> input) {
+        String diskMap = input.findFirst().orElseThrow();
+        char[] charArray = diskMap.toCharArray();
+
+        int fileId = 0;
+        List<Block> blocks = new ArrayList<>();
+        Map<Integer, EmptySpace> freeBlocks = new TreeMap<>();
+        for (int i = 0; i < charArray.length; i++) {
+            int size = Character.digit(charArray[i], 10);
+            if (i % 2 == 0) {
+                for (int n = 0; n < size; n++) {
+                    blocks.add(new FileBlock(fileId, size));
+                }
+                fileId++;
+            } else {
+                int blockStart = blocks.size();
+                for (int n = 0; n < size; n++) {
+                    blocks.add(new EmptySpace(size));
+                }
+                if (size > 0) {
+                    freeBlocks.put(blockStart, new EmptySpace(size));
+                }
             }
         }
 
-        return checksum; // Your puzzle answer was 6382875730645
+        Set<Integer> moved = new HashSet<>();
+
+        for (int i = blocks.size() - 1; i >= 1; i--) {
+            Block block = blocks.get(i);
+            if (!(block instanceof FileBlock fileBlock)) {
+                continue;
+            }
+
+            if (moved.contains(fileBlock.id())) {
+                continue;
+            }
+
+            Optional<Pair<EmptySpace, Integer>> optionalFirstBigEnoughFreeBlock = findFirstBigEnoughFreeBlock(freeBlocks, fileBlock.size());
+            if (optionalFirstBigEnoughFreeBlock.isEmpty()) {
+                continue;
+            }
+
+            EmptySpace freeBlock = optionalFirstBigEnoughFreeBlock.get().first();
+            Integer freeBlockStart = optionalFirstBigEnoughFreeBlock.get().second();
+
+            if (freeBlockStart >= i) {
+                continue;
+            }
+
+            // Get all the file block parts to move
+            List<FileBlock> fileBlocksToMove = new ArrayList<>(fileBlock.size());
+            for (int j = i - fileBlock.size() + 1; j < i; j++) {
+                fileBlocksToMove.add((FileBlock) blocks.get(j));
+            }
+            fileBlocksToMove.add(fileBlock);
+
+            // Find the start of the preceding free block, if any
+            int startOfFreeBlock = -1;
+            for (int j = i - fileBlock.size(); j >= Math.max(i - fileBlock.size() - 9 - 1, 0); j--) {
+                Block b = blocks.get(j);
+                if (!(b instanceof EmptySpace)) {
+                    startOfFreeBlock = j + 1;
+                    break;
+                }
+            }
+
+            // Find the end of the following free block, if any
+            int endOfFreeBlock = -1;
+            for (int j = i + 1; j < Math.min(i + 9 + 1, blocks.size()); j++) {
+                Block b = blocks.get(j);
+                if (!(b instanceof EmptySpace)) {
+                    endOfFreeBlock = j - 1;
+                    break;
+                }
+            }
+            if (endOfFreeBlock == -1) {
+                // No free block following this block, only mark free until the end of the file block.
+                endOfFreeBlock = i;
+            }
+
+            if (startOfFreeBlock != -1) {
+                EmptySpace newEmptySpace = new EmptySpace(endOfFreeBlock - startOfFreeBlock + 1);
+                for (int j = startOfFreeBlock; j <= endOfFreeBlock; j++) {
+                    blocks.set(j, newEmptySpace);
+                }
+                freeBlocks.put(startOfFreeBlock, newEmptySpace);
+                freeBlocks.remove(i + 1);
+            }
+
+            int blocksToMove = fileBlocksToMove.size();
+            for (int j = freeBlockStart; j < freeBlockStart + blocksToMove; j++) {
+                if (!(blocks.get(j) instanceof EmptySpace)) {
+                    throw new IllegalStateException();
+                }
+                blocks.set(j, fileBlocksToMove.removeFirst());
+            }
+
+            if (!fileBlocksToMove.isEmpty()) {
+                throw new IllegalStateException();
+            }
+
+            if (freeBlock.size() > fileBlock.size()) {
+                EmptySpace remainingFreeSpace = new EmptySpace(freeBlock.size() - fileBlock.size());
+                for (int j = freeBlockStart + fileBlock.size(); j < freeBlockStart + freeBlock.size(); j++) {
+                    blocks.set(j, remainingFreeSpace);
+                }
+                freeBlocks.put(freeBlockStart + fileBlock.size(), remainingFreeSpace);
+            }
+
+            freeBlocks.remove(freeBlockStart);
+
+            moved.add(fileBlock.id());
+        }
+
+        return calculateChecksum(blocks); // Your puzzle answer was 6420913943576
+    }
+
+    private static long calculateChecksum(List<Block> blocks) {
+        long checksum = 0;
+        for (int i = 0; i < blocks.size(); i++) {
+            Block block = blocks.get(i);
+            if (block instanceof FileBlock(int id, int _)) {
+                checksum += (long) id * i;
+            }
+        }
+        return checksum;
     }
 
     private static int getFirstFreePosition(List<Block> blocks, int from) {
@@ -76,11 +201,27 @@ public class Day9 extends Day {
         return -1;
     }
 
-    private interface Block {}
+    private static Optional<Pair<EmptySpace, Integer>> findFirstBigEnoughFreeBlock(Map<Integer, EmptySpace> freeBlocks, int neededSize) {
+        for (var entry : freeBlocks.entrySet()) {
+            EmptySpace freeBlock = entry.getValue();
+            Integer freeBlockStart = entry.getKey();
+            if (freeBlock.size() < neededSize) {
+                continue;
+            }
 
-    record FileBlock(int id) implements Block{
+            return Optional.of(Pair.of(freeBlock, freeBlockStart));
+        }
+
+        return Optional.empty();
     }
 
-    record EmptySpace() implements Block {
+    private interface Block {
+        int size();
+    }
+
+    record FileBlock(int id, int size) implements Block {
+    }
+
+    record EmptySpace(int size) implements Block {
     }
 }
