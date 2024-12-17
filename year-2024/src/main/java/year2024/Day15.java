4587fcf48b6c93ffe0ee88a95681806f67007ce5
Sebastian Lövdahl <slovdahl@hibox.fi>
2024-12-17 22:12:53 +0200

    WIP

--- year-2024/src/main/java/year2024/Day15.java
+++ year-2024/src/main/java/year2024/Day15.java
@@ -14,17 +14,24 @@ import static com.google.common.base.Preconditions.checkNotNull;
 @SuppressWarnings("unused")
 public class Day15 extends Day {
 
+    private char[][] map;
+    private String moves;
+
     @Override
     protected Mode mode() {
-        return Mode.REAL_INPUT;
+        return Mode.SAMPLE_INPUT;
+    }
+
+    @Override
+    protected void prepare(Stream<String> input) {
+        List<List<String>> sections = Parse.sections(input);
+        map = Matrix.matrix(sections.getFirst());
+        moves = String.join("", sections.getLast());
     }
 
     @Override
     protected Object part1(Stream<String> input) {
-        List<List<String>> sections = Parse.sections(input);
-        char[][] map = Matrix.matrix(sections.getFirst());
-        String moves = String.join("", sections.getLast());
-
+        char[][] map = Matrix.deepClone(this.map);
         Coordinate position = Matrix.findChar(map, '@');
         checkNotNull(position);
 
@@ -44,6 +51,61 @@ public class Day15 extends Day {
                 .sum();
     }
 
+    @Override
+    protected Object part2(Stream<String> input) throws Exception {
+        growMap();
+
+        Coordinate position = Matrix.findChar(map, '@');
+
+
+        for (char c : moves.toCharArray()) {
+            position = switch (c) {
+                case '^' -> moveToNextPosition2(position, map, Direction.UP);
+                case '>' -> moveToNextPosition2(position, map, Direction.RIGHT);
+                case 'v' -> moveToNextPosition2(position, map, Direction.DOWN);
+                case '<' -> moveToNextPosition2(position, map, Direction.LEFT);
+                default -> throw new IllegalStateException();
+            };
+        }
+
+        Matrix.print(System.out, map);
+
+        return Matrix.findChars(map, '[')
+                .stream()
+                .mapToLong(c -> c.row() * 100L + c.column())
+                .sum();
+    }
+
+    private void growMap() {
+        for (int i = 0; i < map.length; i++) {
+            char[] oldRow = map[i];
+            map[i] = new char[oldRow.length * 2];
+            for (int j = 0; j < oldRow.length; j++) {
+                switch (oldRow[j]) {
+                    case '#' -> {
+                        map[i][j * 2] = '#';
+                        map[i][j * 2 + 1] = '#';
+                    }
+
+                    case 'O' -> {
+                        map[i][j * 2] = '[';
+                        map[i][j * 2 + 1] = ']';
+                    }
+
+                    case '.' -> {
+                        map[i][j * 2] = '.';
+                        map[i][j * 2 + 1] = '.';
+                    }
+
+                    case '@' -> {
+                        map[i][j * 2] = '@';
+                        map[i][j * 2 + 1] = '.';
+                    }
+                }
+            }
+        }
+    }
+
     private static Coordinate moveToNextPosition(Coordinate robotPosition, char[][] map, Direction direction) {
         char ch = robotPosition.atDirection(map, direction, '#');
         if (ch == '#') {
@@ -52,12 +114,52 @@ public class Day15 extends Day {
 
         Coordinate newPosition = robotPosition.move(direction);
         Coordinate current = newPosition;
-        Coordinate lastFreeSpot = null;
+        Coordinate freeSpot = null;
         int boxes = 0;
 
         while (ch != '#') {
             if (ch == '.') {
-                lastFreeSpot = current;
+                freeSpot = current;
+                break;
+            } else if (ch == '[' || ch == ']') { // TODO: need to rewrite this completely
+                boxes++;
+            } else {
+                throw new IllegalStateException();
+            }
+
+            current = current.move(direction);
+            ch = current.at(map);
+        }
+
+        if (freeSpot != null) {
+            for (int i = 0; i < boxes; i++) {
+                freeSpot.set(map, 'O');
+                freeSpot = freeSpot.move(direction.opposite());
+            }
+
+            newPosition.set(map, '@');
+            robotPosition.set(map, '.');
+
+            return newPosition;
+        } else {
+            return robotPosition;
+        }
+    }
+
+    private static Coordinate moveToNextPosition2(Coordinate robotPosition, char[][] map, Direction direction) {
+        char ch = robotPosition.atDirection(map, direction, '#');
+        if (ch == '#') {
+            return robotPosition;
+        }
+
+        Coordinate newPosition = robotPosition.move(direction);
+        Coordinate current = newPosition;
+        Coordinate freeSpot = null;
+        int boxes = 0;
+
+        while (ch != '#') {
+            if (ch == '.') {
+                freeSpot = current;
                 break;
             } else if (ch == 'O') {
                 boxes++;
@@ -69,10 +171,10 @@ public class Day15 extends Day {
             ch = current.at(map);
         }
 
-        if (lastFreeSpot != null) {
+        if (freeSpot != null) {
             for (int i = 0; i < boxes; i++) {
-                lastFreeSpot.set(map, 'O');
-                lastFreeSpot = lastFreeSpot.move(direction.opposite());
+                freeSpot.set(map, 'O');
+                freeSpot = freeSpot.move(direction.opposite());
             }
 
             newPosition.set(map, '@');
