91ca6c344b5874b1f4fb9b718fa39589652d3020
Sebastian Lövdahl <slovdahl@hibox.fi>
2024-12-18 22:36:21 +0200

    WIP

--- year-2024/src/main/java/year2024/Day15.java
+++ year-2024/src/main/java/year2024/Day15.java
@@ -4,12 +4,15 @@ import lib.Coordinate;
 import lib.Day;
 import lib.Direction;
 import lib.Matrix;
+import lib.Pair;
 import lib.Parse;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Stream;
-
-import static com.google.common.base.Preconditions.checkNotNull;
 
 @SuppressWarnings("unused")
 public class Day15 extends Day {
@@ -33,17 +36,8 @@ public class Day15 extends Day {
     protected Object part1(Stream<String> input) {
         char[][] map = Matrix.deepClone(this.map);
         Coordinate position = Matrix.findChar(map, '@');
-        checkNotNull(position);
 
-        for (char c : moves.toCharArray()) {
-            position = switch (c) {
-                case '^' -> moveToNextPosition(position, map, Direction.UP);
-                case '>' -> moveToNextPosition(position, map, Direction.RIGHT);
-                case 'v' -> moveToNextPosition(position, map, Direction.DOWN);
-                case '<' -> moveToNextPosition(position, map, Direction.LEFT);
-                default -> throw new IllegalStateException();
-            };
-        }
+        applyMoves(position, map);
 
         return Matrix.findChars(map, 'O')
                 .stream()
@@ -51,23 +45,149 @@ public class Day15 extends Day {
                 .sum(); // Your puzzle answer was 1429911
     }
 
+    @Override
+    protected Object part2(Stream<String> input) throws Exception {
+        growMap();
+        Coordinate position = Matrix.findChar(map, '@');
+
+        applyMoves(position, map);
+
+        Matrix.print(System.out, map);
+
+        return Matrix.findChars(map, '[')
+                .stream()
+                .mapToLong(c -> c.row() * 100L + c.column())
+                .sum(); // Your puzzle answer was XXXX
+        // 1434730 too low
+    }
+
+    private void applyMoves(Coordinate position, char[][] map) {
+        // TODO: clean up once debugging is done
+        char[] charArray = moves.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            position = switch (c) {
+                case '^' -> moveToNextPosition(position, map, Direction.UP);
+                case '>' -> moveToNextPosition(position, map, Direction.RIGHT);
+                case 'v' -> moveToNextPosition(position, map, Direction.DOWN);
+                case '<' -> moveToNextPosition(position, map, Direction.LEFT);
+                default -> throw new IllegalStateException();
+            };
+
+            for (Coordinate ch : Matrix.findChars(map, '[')) {
+                if (ch.moveRight().at(map) != ']') {
+                    throw new IllegalStateException();
+                }
+            }
+        }
+    }
+
+    private void growMap() {
+        for (int i = 0; i < map.length; i++) {
+            char[] oldRow = map[i];
+            map[i] = new char[oldRow.length * 2];
+            for (int j = 0; j < oldRow.length; j++) {
+                switch (oldRow[j]) {
+                    case '#' -> {
+                        map[i][j * 2] = '#';
+                        map[i][j * 2 + 1] = '#';
+                    }
+
+                    case 'O' -> {
+                        map[i][j * 2] = '[';
+                        map[i][j * 2 + 1] = ']';
+                    }
+
+                    case '.' -> {
+                        map[i][j * 2] = '.';
+                        map[i][j * 2 + 1] = '.';
+                    }
+
+                    case '@' -> {
+                        map[i][j * 2] = '@';
+                        map[i][j * 2 + 1] = '.';
+                    }
+                }
+            }
+        }
+    }
+
     private static Coordinate moveToNextPosition(Coordinate robotPosition, char[][] map, Direction direction) {
-        char ch = robotPosition.atDirection(map, direction, '#');
-        if (ch == '#') {
+        Map<Coordinate, Integer> sidewaysStartingPoints = new HashMap<>();
+        Pair<Boolean, Integer> result = canShiftOneStep(robotPosition, direction, map, sidewaysStartingPoints, new HashSet<>());
+        if (!result.first()) {
             return robotPosition;
         }
 
-        Coordinate newPosition = robotPosition.move(direction);
-        Coordinate current = newPosition;
-        Coordinate freeSpot = null;
-        int boxes = 0;
+        shift(map, robotPosition, direction, result.second());
 
+        for (var entry : sidewaysStartingPoints.entrySet()) {
+            shift(map, entry.getKey(), direction, entry.getValue());
+        }
+
+        return robotPosition.move(direction);
+    }
+
+    private static Pair<Boolean, Integer> canShiftOneStep(Coordinate from,
+                                                          Direction direction,
+                                                          char[][] map,
+                                                          Map<Coordinate, Integer> sidewaysStartingPoints,
+                                                          Set<Integer> checkedColumns) {
+
+        checkedColumns.add(from.column());
+
+        char ch = from.atDirection(map, direction, '#');
+        Coordinate current = from.move(direction);
+        int toMove = 0;
+
+        /*
+        TODO: Handle this case, Direction.DOWN:
+
+        ....[].....
+        ...[].@....
+        .....[].##.
+        ....[][]...
+        ##..[].....
+        []..[]##[].
+        [].[][].[].
+        ....[].....
+        ..........#
+         */
         while (ch != '#') {
             if (ch == '.') {
-                freeSpot = current;
-                break;
+                return Pair.of(true, toMove);
             } else if (ch == 'O') {
-                boxes++;
+                toMove++;
+            } else if (ch == '[' || ch == ']') {
+                if (direction == Direction.UP || direction == Direction.DOWN) {
+                    toMove++;
+
+                    Direction checkDirection;
+                    if (ch == ']') {
+                        checkDirection = Direction.LEFT;
+                    } else {
+                        checkDirection = Direction.RIGHT;
+                    }
+
+                    Coordinate move = current.move(checkDirection);
+                    if (!checkedColumns.contains(move.column())) {
+                        Pair<Boolean, Integer> canMoveOneStep = canShiftOneStep(
+                                move,
+                                direction,
+                                map,
+                                sidewaysStartingPoints,
+                                checkedColumns
+                        );
+
+                        if (!canMoveOneStep.first()) {
+                            return Pair.of(false, 0);
+                        }
+
+                        sidewaysStartingPoints.put(move, canMoveOneStep.second());
+                    }
+                } else {
+                    toMove++;
+                }
             } else {
                 throw new IllegalStateException();
             }
@@ -76,18 +196,17 @@ public class Day15 extends Day {
             ch = current.at(map);
         }
 
-        if (freeSpot != null) {
-            for (int i = 0; i < boxes; i++) {
-                freeSpot.set(map, 'O');
-                freeSpot = freeSpot.move(direction.opposite());
-            }
+        return Pair.of(false, 0);
+    }
 
-            newPosition.set(map, '@');
-            robotPosition.set(map, '.');
-
-            return newPosition;
-        } else {
-            return robotPosition;
+    private static void shift(char[][] map, Coordinate from, Direction direction, int n) {
+        Coordinate freeSpot = from.move(direction, n + 1);
+        for (int i = 0; i <= n; i++) {
+            Coordinate next = freeSpot.move(direction.opposite());
+            char newChar = next.at(map);
+            freeSpot.set(map, newChar);
+            freeSpot = next;
         }
+        from.set(map, '.');
     }
 }
