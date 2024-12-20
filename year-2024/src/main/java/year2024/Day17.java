a6efa41566a3edcefd00c2fab697c1ed875a0c5f
Sebastian Lövdahl <slovdahl@hibox.fi>
2024-12-21 00:06:48 +0200

    experimentation

--- year-2024/src/main/java/year2024/Day17.java
+++ year-2024/src/main/java/year2024/Day17.java
@@ -1,12 +1,17 @@
 package year2024;
 
+import com.google.common.primitives.Ints;
 import lib.Day;
 import lib.Parse;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkState;
@@ -15,9 +20,10 @@ import static java.util.stream.Collectors.joining;
 @SuppressWarnings("unused")
 public class Day17 extends Day {
 
-    private int registerA;
-    private int registerB;
-    private int registerC;
+    private Map<String, Integer> registerSection;
+    private int initialRegisterA;
+    private int initialRegisterB;
+    private int initialRegisterC;
     private List<Integer> program;
 
     @Override
@@ -35,11 +41,11 @@ public class Day17 extends Day {
             checkState(matcher.find());
 
             if (matcher.group(1).equals("A")) {
-                registerA = Integer.parseInt(matcher.group(2));
+                initialRegisterA = Integer.parseInt(matcher.group(2));
             } else if (matcher.group(1).equals("B")) {
-                registerB = Integer.parseInt(matcher.group(2));
+                initialRegisterB = Integer.parseInt(matcher.group(2));
             } else if (matcher.group(1).equals("C")) {
-                registerC = Integer.parseInt(matcher.group(2));
+                initialRegisterC = Integer.parseInt(matcher.group(2));
             } else {
                 throw new IllegalArgumentException("Unknown register '" + registerString + "'");
             }
@@ -51,43 +57,42 @@ public class Day17 extends Day {
 
     @Override
     protected Object part1(Stream<String> input) {
+        long registerA = initialRegisterA;
+        long registerB = initialRegisterB;
+        long registerC = initialRegisterC;
         int instructionPointer = 0;
         List<Integer> out = new ArrayList<>();
 
         while (true) {
-            Integer instruction;
-            try {
-                instruction = program.get(instructionPointer);
-            } catch (IndexOutOfBoundsException e) {
+            if (instructionPointer >= program.size()) {
                 break;
             }
+            Integer instruction = program.get(instructionPointer);
+            Integer operand = program.get(instructionPointer + 1);
 
             switch (instruction) {
                 // adv
                 case 0 -> {
-                    Integer operand = program.get(instructionPointer + 1);
-                    registerA = registerA / (int) Math.pow(2, comboOperand(operand));
+                    registerA = registerA / (int) Math.pow(2, comboOperand(operand, registerA, registerB, registerC));
                     instructionPointer += 2;
                 }
 
                 // bxl
                 case 1 -> {
-                    Integer operand = program.get(instructionPointer + 1);
                     registerB = registerB ^ operand;
                     instructionPointer += 2;
                 }
 
                 // bst
                 case 2 -> {
-                    Integer operand = program.get(instructionPointer + 1);
-                    registerB = comboOperand(operand) % 8;
+                    registerB = comboOperand(operand, registerA, registerB, registerC) % 8L;
                     instructionPointer += 2;
                 }
 
                 // jnz
                 case 3 -> {
                     if (registerA != 0) {
-                        instructionPointer = program.get(instructionPointer + 1);
+                        instructionPointer = operand;
                     } else {
                         instructionPointer += 2;
                     }
@@ -101,22 +106,20 @@ public class Day17 extends Day {
 
                 // out
                 case 5 -> {
-                    Integer operand = program.get(instructionPointer + 1);
-                    out.add(comboOperand(operand) % 8);
+                    // TODO: fix
+                    out.add((int)(comboOperand(operand, registerA, registerB, registerC) % 8));
                     instructionPointer += 2;
                 }
 
                 // bdv
                 case 6 -> {
-                    Integer operand = program.get(instructionPointer + 1);
-                    registerB = registerA / (int) Math.pow(2, comboOperand(operand));
+                    registerB = registerA / (int) Math.pow(2, comboOperand(operand, registerA, registerB, registerC));
                     instructionPointer += 2;
                 }
 
                 // cdv
                 case 7 -> {
-                    Integer operand = program.get(instructionPointer + 1);
-                    registerC = registerA / (int) Math.pow(2, comboOperand(operand));
+                    registerC = registerA / (int) Math.pow(2, comboOperand(operand, registerA, registerB, registerC));
                     instructionPointer += 2;
                 }
 
@@ -129,16 +132,171 @@ public class Day17 extends Day {
                 .collect(joining(",")); // Your puzzle answer was 6,7,5,2,1,3,5,1,7
     }
 
-    private int comboOperand(int operand) {
+    @Override
+    protected Object part2(Stream<String> input) {
+        /*
+        0: 2,4
+        1: 1,3
+        2: 7,5
+        3: 1,5
+        4: 0,3
+        5: 4,1
+        6: 5,5
+        7: 3,0
+         */
+
+        /*
+        0 output : [6]
+        1 output : [6]
+        2 output : [6]
+        3 output : [0]
+        4 output : [2]
+        5 output : [3]
+        6 output : [0]
+        7 output : [1]
+
+        100 output : [2]
+        101 output : [3]
+        102 output : [0]
+        103 output : [1]
+        104 output : [6]
+        105 output : [6]
+        106 output : [6]
+        107 output : [0]
+         */
+
+        /*
+        4 output : [2]
+        2751 output : [2, 4, 1, 3]
+        6816 output : [2, 4, 1, 3, 7]
+        6847 output : [2, 4, 1, 3, 7]
+        A: 470000000
+        A: 60000000
+        105995885 output : [2, 4, 1, 3, 7, 5, 1, 5, 0]
+        105995967 output : [2, 4, 1, 3, 7, 5, 1, 5, 0]
+        106126957 output : [2, 4, 1, 3, 7, 5, 1, 5, 0]
+        106127039 output : [2, 4, 1, 3, 7, 5, 1, 5, 0]
+         */
+
+        // tested until 13180000000
+        // tested until 14080000000
+        // tested until 20000000000
+        // tested until 35300000000
+        return LongStream.iterate(35_300_000_000L, i -> i + 1)
+                .parallel()
+                .map(i -> {
+                            if (i % 100_000_000 == 0) {
+                                System.out.println("A: " + i);
+                            }
+
+                            long registerA = i;
+                            long registerB = initialRegisterB;
+                            long registerC = initialRegisterC;
+                            int instructionPointer = 0;
+                            List<Integer> out = new ArrayList<>();
+
+                            while (true) {
+                                if (instructionPointer >= program.size()) {
+                                    break;
+                                }
+                                Integer instruction = program.get(instructionPointer);
+                                Integer operand = program.get(instructionPointer + 1);
+
+                                switch (instruction) {
+                                    // adv
+                                    case 0 -> {
+                                        registerA = division(operand, registerA, registerB, registerC);
+                                        instructionPointer += 2;
+                                    }
+
+                                    // bxl
+                                    case 1 -> {
+                                        registerB = registerB ^ operand;
+                                        instructionPointer += 2;
+                                    }
+
+                                    // bst
+                                    case 2 -> {
+                                        registerB = comboOperand(operand, registerA, registerB, registerC) % 8;
+                                        instructionPointer += 2;
+                                    }
+
+                                    // jnz
+                                    case 3 -> {
+                                        if (registerA != 0) {
+                                            instructionPointer = operand;
+                                        } else {
+                                            instructionPointer += 2;
+                                        }
+                                    }
+
+                                    // bxc
+                                    case 4 -> {
+                                        registerB = registerB ^ registerC;
+                                        instructionPointer += 2;
+                                    }
+
+                                    // out
+                                    case 5 -> {
+                                        long comboOperand = comboOperand(operand, registerA, registerB, registerC);
+                                        out.add((int) (comboOperand % 8));
+
+                                        if (!program.subList(0, out.size()).equals(out) || out.size() > program.size()) {
+                                            // We can fail fast, this output does not match the wanted program
+                                            return -1;
+                                        }
+
+                                        instructionPointer += 2;
+                                    }
+
+                                    // bdv
+                                    case 6 -> {
+                                        registerB = division(operand, registerA, registerB, registerC);
+                                        instructionPointer += 2;
+                                    }
+
+                                    // cdv
+                                    case 7 -> {
+                                        registerC = division(operand, registerA, registerB, registerC);
+                                        instructionPointer += 2;
+                                    }
+
+                                    default -> throw new IllegalStateException("Unexpected value: " + instruction);
+                                }
+                            }
+
+                            System.out.println(i + " output : " + out);
+                            if (out.equals(program)) {
+                                return i;
+                            } else {
+                                //if (out.size() > 1) {
+                                //System.out.println("Out: " + out);
+                                //}
+                                return -1;
+                            }
+                        }
+                )
+                .filter(i -> i >= 0)
+                .findFirst()
+                .orElseThrow();
+    }
+
+    private static long comboOperand(int operand, long registerA, long registerB, long registerC) {
         return switch (operand) {
-            case 0 -> 0;
-            case 1 -> 1;
-            case 2 -> 2;
-            case 3 -> 3;
-            case 4 -> registerA;
-            case 5 -> registerB;
-            case 6 -> registerC;
+            case 0, 1, 2, 3 -> operand;
+            case 4 -> BigInteger.valueOf(registerA).longValueExact();
+            case 5 -> BigInteger.valueOf(registerB).longValueExact();
+            case 6 -> BigInteger.valueOf(registerC).longValueExact();
             default -> throw new IllegalStateException("Unexpected value: " + operand);
         };
+    }
+
+    private static long division(int operand, long registerA, long registerB, long registerC) {
+        long comboOperand = comboOperand(operand, registerA, registerB, registerC);
+        if (comboOperand >= 63) {
+            return 0L;
+        }
+
+        return registerA / (long) Math.pow(2, comboOperand);
     }
 }
